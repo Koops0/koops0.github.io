fbc5475ff7ee144ef00d68f1042019ad938bcaa8
Kershan Arulneswaran <74938655+Koops0@users.noreply.github.com>
2024-06-11 17:06:25 -0400

    needed to use apos

--- components/about.tsx
+++ components/about.tsx
@@ -14,13 +14,13 @@ export default function About() {
             <SectionHeading>About Me</SectionHeading>
             <p className="text-3xl text-white font-inter font-medium mx-16">
             As a third-year undergraduate student pursuing a degree in Computer Science with a minor in 
-            Mathematics at Ontario Tech University, I've cultivated a strong foundation in computer science 
+            Mathematics at Ontario Tech University, I&apos;ve cultivated a strong foundation in computer science 
             and honed my skills in programming languages like Python, Java, and C++. From developing diverse 
-            projects, including a Go emulator and full-stack web applications centered around Chess, I'm adept 
+            projects, including a bot that can play games for me and full-stack web applications centered around Chess, I&apos;m adept 
             at solving complex problems and thrive on adding innovative twists to my work. With a passion for 
             sectors like Computer Vision, Artificial Intelligence, Robotics, and Full-Stack Software Development, 
-            I'm eager to explore exciting opportunities in robotics, software engineering, or ML/AI as a co-op intern. 
-            Committed to collaboration and staying abreast of industry trends, I'm driven to contribute to technological 
+            I&apos;m eager to explore exciting opportunities in robotics, software engineering, or ML/AI as a co-op intern. 
+            Committed to collaboration and staying abreast of industry trends, I&apos;m driven to contribute to technological 
             advancements and eager to connect with professionals.</p>
         </motion.section>
     )
