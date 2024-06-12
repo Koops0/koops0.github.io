7c7b5eb2ad90d5fcc7ae1a3c544adf7534ac29b3
Kershan Arulneswaran <74938655+Koops0@users.noreply.github.com>
2024-06-12 11:56:22 -0400

    fixed mobile design

--- components/intro.tsx
+++ components/intro.tsx
@@ -17,7 +17,7 @@ export default function Intro(){
     const { ref } = useSectionInView("Home", 0.5);
     const isSmallScreen = useMediaQuery({ query: '(max-width: 640px)' })
     return (
-        <section ref={ref} id="home" className="overflow-hidden">
+        <section ref={ref} id="home" className={`overflow-hidden ${isSmallScreen ? 'mt-20' : ''}`}>
             <div className='flex items-center justify-center'>
                 <div className = "relative">
                     <motion.div
@@ -26,14 +26,14 @@ export default function Intro(){
                         transition={{type: 'tween', duration: 0.3}}
                     >
                         <Image src='/kersh.jpeg' alt='Kersh' width="512" height="512" 
-                            quality = "100" priority={true} className='h-64 w-64 sm:h-80 sm:w-80 md:h-96 md:w-96 rounded-3xl
+                            quality = "100" priority={true} className='h-48 w-48 sm:h-64 sm:w-64 md:h-80 md:w-80 lg:h-96 lg:w-96 rounded-3xl
                             object-cover border-[0.2rem] border-white border-opacity-50 
                             shadow-lg shadow-black/[0.03] sm:translate-x-[-25rem] sm:translate-y-[10rem]'
                         />
                     </motion.div>
                 </div>
             </div>
-            <motion.p className='m1-auto text-center text-3xl sm:text-4xl md:text-8xl text-white font-excelorate font-extrabold mt-4'
+            <motion.p className='m1-auto text-center text-2xl sm:text-4xl md:text-8xl text-white font-excelorate font-extrabold mt-4'
             initial={{opacity:0, x: isSmallScreen ? 0 : 250, y: isSmallScreen ? 150 : 100}}
             animate={{opacity:1, x: isSmallScreen ? 0 : 250, y: isSmallScreen ? 0 : -70}}>
                 HELLO WORLD.
@@ -44,25 +44,27 @@ export default function Intro(){
                 My name is Kershan Arulneswaran.
             </motion.p>
             <motion.div className="flex flex-col sm:flex-row justify-center gap-2 transform translate-x-64 -translate-y-14"
-            initial={{opacity:0, x: isSmallScreen ? 0 : 250, y:100}}
-            animate={{opacity:1, x: isSmallScreen ? 0 : 250, y:-70}}
-            transition={{delay:0.1}}>
+                        initial={{opacity:0, x: isSmallScreen ? 0 : 250, y: isSmallScreen ? 150 : 100}}
+                        animate={{opacity:1, x: isSmallScreen ? 0 : 250, y: isSmallScreen ? 0 : -70}}
+                        transition={{delay:0.1}}>
+                <div className="flex justify-center gap-2">
                 <Link href = "#contact"
-                className="group bg-white bg-opacity-60 text-gray-950 font-inter px-4 sm:px-7 py-2 sm:py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-slate-50">
+                      className="group bg-white bg-opacity-60 text-gray-950 font-inter px-2 sm:px-7 py-2 sm:py-3 flex items-center justify-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-slate-50 w-24 h-12 sm:w-auto sm:h-auto">
                     Contact Me <BsArrowDown className="group-hover:translate-y-1 transition"/>
                 </Link>
                 <Link href="https://drive.google.com/file/d/1y53pLRKB6lXpBrqveCbhOjl8K0FjnPP1/view?usp=sharing" 
-                className="group bg-gray-950 bg-opacity-60 text-white font-inter px-4 sm:px-7 py-2 sm:py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-black">
+                      className="group bg-gray-950 bg-opacity-60 text-white font-inter px-2 sm:px-7 py-2 sm:py-3 flex items-center jestify-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-black w-24 h-12 sm:w-auto sm:h-auto">
                     Resume <HiDownload className="group-hover:translate-y-1 transition"/>
                 </Link>
                 <Link href = "https://www.linkedin.com/in/kersharul/"
-                className="bg-white bg-opacity-60 text-gray-950 font-inter p-3 sm:p-4 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-slate-50">
+                      className="bg-white bg-opacity-60 text-gray-950 font-inter p-2 sm:p-4 flex items-center justify-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-slate-50 w-24 h-12 sm:w-auto sm:h-auto">
                     <FaLinkedin/>
                 </Link>
                 <Link href = "https://github.com/Koops0"
-                className="bg-white bg-opacity-60 text-gray-950 font-inter p-3 sm:p-4 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-slate-50">
+                      className="bg-white bg-opacity-60 text-gray-950 font-inter p-2 sm:p-4 flex items-center justify-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-slate-50 w-24 h-12 sm:w-auto sm:h-auto">
                     <FaGithubSquare/>
                 </Link>
+                </div>
             </motion.div>
         </section>
     );
